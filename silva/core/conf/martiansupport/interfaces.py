6973e567fb8e750b5d511d464c78c33ec44c6554
Sylvain Viollon <thefunny@gmail.com>
2008-08-18 12:43:50 +0000

    - Add a grokker to register layer/customizable interfaces.

---
+++ silva/core/conf/martiansupport/interfaces.py
@@ -0,0 +1,38 @@
+# Copyright (c) 2002-2008 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id$
+
+from zope.interface.interface import InterfaceClass
+from zope.component.interface import provideInterface
+
+from Products.SilvaLayout.interfaces import ISilvaLayer, ISilvaSkin
+from Products.Silva.interfaces import ISilvaObject
+from silva.core.views.interfaces import ISilvaCustomizableType, ISilvaLayerType
+
+import martian
+
+class SilvaInterfaceGrokker(martian.InstanceGrokker):
+    """We register interfaces than people can customize as utility on
+    ISilvaCustomizable.
+    """
+
+    martian.component(InterfaceClass)
+
+    def grok(self, name, interface, module_info, config, **kw):
+
+        if interface.isOrExtends(ISilvaObject):
+            config.action(
+                discriminator=('utility', ISilvaCustomizableType, interface),
+                callable=provideInterface,
+                args=('', interface, ISilvaCustomizableType))
+            return True
+        if interface.isOrExtends(ISilvaLayer) and not interface.isOrExtends(ISilvaSkin):
+            config.action(
+                discriminator=('utility', ISilvaLayerType, interface),
+                callable=provideInterface,
+                args=('', interface, ISilvaLayerType))
+            return True
+        return False
+
+                
+
