c5b76bb63c69eb85494ab7b8add1e16c7833caa7
Sylvain Viollon <thefunny@gmail.com>
2009-07-31 08:39:53 +0000

    add helpers configure and unconfigure metadata.

--- silva/core/conf/installer.py
+++ silva/core/conf/installer.py
@@ -62,3 +62,32 @@ class DefaultInstaller(object):
     def is_installed(self, root):
         return self._interface.providedBy(root.service_extensions)
 
+    def configure_metadata(root, mapping):
+        product = package_home(globals())
+        schema = path.join(product, 'schema')
+        collection = root.service_metadata.getCollection()
+
+        for types, setids in mapping.items():
+            for setid in setids:
+                if not setid in collection.objectIds():
+                    xmlfile = path.join(schema, setid+'.xml')
+                    definition = open(xmlfile, 'r')
+                    collection.importSet(definition)
+            root.service_metadata.addTypesMapping(types, setids)
+        root.service_metadata.initializeMetadata()
+
+    def unconfigure_metadata(root, mapping):
+        all_types = []
+        all_sets = []
+        for types, setids in mapping.items():
+            for typename in types:
+                if typename in all_types:
+                    continue
+                all_types.append(typename)
+            for setid in setids:
+                if setid in all_sets:
+                    continue
+                all_sets.append(setid)
+        root.service_metadata.removeTypesMapping(all_types, all_sets)
+        root.service_metadata.initializeMetadata()
+
