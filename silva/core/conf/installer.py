932b3c82608286320b63f6fd9bc82a9ad2f8e0b0
Sylvain Viollon <thefunny@gmail.com>
2009-09-17 17:05:17 +0000

    Merge branch 2.1 back with trunk.

--- silva/core/conf/installer.py
+++ silva/core/conf/installer.py
@@ -31,6 +31,7 @@ class SystemExtensionInstaller(object):
     def is_installed(self, root):
         return True
 
+
 class DefaultInstaller(object):
     """Default installer for extension.
     """
@@ -106,8 +107,45 @@ class DefaultInstaller(object):
     def is_installed(self, root):
         return self._interface.providedBy(root.service_extensions)
 
+    def configureMetadata(self, root, mapping, where=None):
+        if where is None:
+            where = globals()
+        product = package_home(where)
+        schema = os.path.join(product, 'schema')
+        collection = root.service_metadata.getCollection()
 
-    # Helpers
+        for types, setids in mapping.items():
+            for setid in setids:
+                if not setid in collection.objectIds():
+                    xmlfile = os.path.join(schema, setid+'.xml')
+                    definition = open(xmlfile, 'r')
+                    collection.importSet(definition)
+            root.service_metadata.addTypesMapping(types, setids)
+        root.service_metadata.initializeMetadata()
+
+    def unconfigureMetadata(self, root, mapping):
+        all_types = []
+        all_sets = []
+        for types, setids in mapping.items():
+            for typename in types:
+                if typename in all_types:
+                    continue
+                all_types.append(typename)
+            for setid in setids:
+                if setid in all_sets:
+                    continue
+                all_sets.append(setid)
+        root.service_metadata.removeTypesMapping(all_types, all_sets)
+        root.service_metadata.initializeMetadata()
+        # delete metadata description if there
+        collection = root.service_metadata.getCollection()
+        setids = set()
+        for values in mapping.values():
+            setids.update(values)
+        for setid in setids:
+            if hasattr(collection, setid):
+                # TODO check if setid is still used somewhere
+                collection.manage_delObjects([setid,])
 
     @zope.cachedescriptors.property.CachedProperty
     def extension(self):
