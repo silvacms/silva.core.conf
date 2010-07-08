8e8715b222534e77bbfb4ec05a213ac53b58c444
Sylvain Viollon <thefunny@gmail.com>
2010-07-08 11:16:08 +0000

    Add interfaces.

---
+++ src/silva/core/conf/interfaces.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2002-2010 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id$
+
+from silva.core.conf.interfaces import ID
+from silva.translations import translate as _
+from zope.interface import Interface
+
+
+class IIdentifiedContent(Interface):
+    """A content with an identifier.
+    """
+    id = ID(
+        title=_(u"id"),
+        description=_(u"No spaces or special characters besides "
+                      u"‘_’ or ‘-’ or ‘.’"),
+        required=True)
+
+
+class ITitledContent(IIdentifiedContent):
+    """A content with an identifier and a title.
+    """
+    title = schema.TextLine(
+        title=_(u"title"),
+        description=_(u"The title will be publicly visible, "
+                      u"and is used for the link in indexes."),
+        required=True)
