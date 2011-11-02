7591787c0e7520480c225c2474b14c82356993cc
Sylvain Viollon <thefunny@gmail.com>
2011-11-02 18:05:12 +0100

    Add a decorator that check a permission on an adapter.

--- src/silva/core/conf/__init__.py
+++ src/silva/core/conf/__init__.py
@@ -11,3 +11,24 @@ from grokcore import component
 from grokcore.view import template, templatedir, require
 
 from zope.interface import implements
+
+
+def protect(permission):
+    """Protect an adapter method with a permission.
+    """
+    from AccessControl.security import checkPermission
+    from zExceptions import Unauthorized
+
+    def wrapper(func):
+
+        def wrapped(self, *args, **kwargs):
+            if not checkPermission(permission, self.context):
+                raise Unauthorized(
+                    "You don't have access to %s on %s" % (
+                        func.func_name,
+                        '/'.join(self.context.getPhysicalPath())))
+            return func(self, *args, **kwargs)
+
+        return wrapped
+
+    return wrapper
